95dde4611a885581e50a3a46df38f6a35c3cc2a8
kpekk <kristjan.pekk@gmail.com>
2020-12-08 19:51:13 +0200

    laikimine korras???

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -68,7 +68,7 @@ router.post('/', authorize,  (request, response) => {
 router.put('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to like a post
-    PostModel.like(request.currentUser.id, request.body.id, (post) =>{
+    PostModel.like(request.currentUser.id, request.params.postId, (post) =>{
         response.status(200).json(post)
     })
 });
@@ -76,7 +76,7 @@ router.put('/:postId/likes', authorize, (request, response) => {
 router.delete('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to unlike a post
-    PostModel.unlike(request.currentUser.id, request.body.id, (post)=>{
+    PostModel.unlike(request.currentUser.id, request.params.postId, (post)=>{
         response.status(200).json(post)
     })
 });
